7354bc8339da956eee92f125030d339172abb548
Arkar Min Aung <arica.aung@gmail.com>
2023-06-30 11:54:01 +0800

    test: make a smaller model for unit test

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -3,21 +3,21 @@ from transformer import Transformer
 
 class TestTransformer(unittest.TestCase):
     def setUp(self):
-        self.transformer = Transformer(d_model=512, nhead=8, num_encoder_layers=6,
-                                       num_decoder_layers=6, dim_feedforward=2048)
+        self.transformer = Transformer(d_model=16, nhead=2, num_encoder_layers=2,
+                                       num_decoder_layers=2, dim_feedforward=128)
 
     def test_model_creation(self):
         self.assertIsNotNone(self.transformer, "Model creation failed.")
 
     def test_model_forward_pass(self):
         # Assuming your input is of shape (batch_size, sequence_length)
-        input_tensor = torch.rand(32, 10, 512)
+        input_tensor = torch.rand(8, 10, 16)
         output = self.transformer(input_tensor)
-        self.assertEqual(output.shape, (32, 10, 512), "Forward pass output shape is incorrect.")
+        self.assertEqual(output.shape, (8, 10, 16), "Forward pass output shape is incorrect.")
 
     def test_model_backward_pass(self):
         # Assuming your input is of shape (batch_size, sequence_length)
-        input_tensor = torch.rand(32, 10, 512)
+        input_tensor = torch.rand(8, 10, 16)
         output = self.transformer(input_tensor)
         output.mean().backward()  # Backward pass
         for name, param in self.transformer.named_parameters():
